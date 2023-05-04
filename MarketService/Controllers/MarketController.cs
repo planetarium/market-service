10da557c3fe07a8f60d9a341b326bdabe6e17cca
Yang Chun Ung <qooraven@gmail.com>
2023-05-04 11:56:10 +0900

    Introduce multiple ticker api

--- MarketService/Controllers/MarketController.cs
+++ MarketService/Controllers/MarketController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Nekoyume.Model.Item;
 using Nekoyume.Model.Stat;
+using Npgsql;
 
 namespace MarketService.Controllers;
 
