d1eda0915ba7ba2efbfc7918907964a1b65b7186
unengine <unengine@gmail.com>
2023-05-24 13:26:39 +0900

    Add migrations

---
+++ MarketService/Migrations/20230524042246_v200020Skill.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketService.Migrations
+{
+    /// <inheritdoc />
+    public partial class v200020Skill : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "referencedstattype",
+                table: "skillmodel",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "statpowerratio",
+                table: "skillmodel",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "referencedstattype",
+                table: "skillmodel");
+
+            migrationBuilder.DropColumn(
+                name: "statpowerratio",
+                table: "skillmodel");
+        }
+    }
+}
